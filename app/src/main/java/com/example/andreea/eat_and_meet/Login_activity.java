22b79000728dc2f4ea1d2baeed2817d03f739bae
andreturtoiu <andreturtoiu@hotmail.it>
2018-01-31 15:38:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/andreea/eat_and_meet/Login_activity.java
@@ -0,0 +1,17 @@
+package com.example.andreea.eat_and_meet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andreea on 30/01/2018.
+ */
+
+public class Login_activity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
