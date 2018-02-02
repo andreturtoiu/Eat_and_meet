1fd3a4f94c547aa786470fdb72d3db16c6ff14e9
andreturtoiu <andreturtoiu@hotmail.it>
2018-02-02 14:49:25 +0100

    login and signup not complete

---
+++ app/src/main/java/com/example/andreea/eat_and_meet/SignUp.java
@@ -0,0 +1,30 @@
+package com.example.andreea.eat_and_meet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Andreea on 31/01/2018.
+ */
+
+public class SignUp extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        findViewById(R.id.signUpConfirm).setOnClickListener(new SignUp.handleButton());
+        findViewById(R.id.imageButton).setOnClickListener(new SignUp.handleButton());
+    }
+
+    class handleButton implements View.OnClickListener {
+        public void onClick(View v) {
+            Intent intent = new Intent(SignUp.this,Login_activity.class);
+            startActivity(intent);
+        }
+    }
+
+}
+
+
