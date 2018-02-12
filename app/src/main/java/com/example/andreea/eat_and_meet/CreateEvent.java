edf42957d2b895edc9a3edeea72bede3fad741a7
Riccardo <r.mame96@gmail.com>
2018-02-12 12:26:59 +0100

    aggiunto data picker a CreateEvent, alcuni fix al layout ed è possibile accedere a crea evento dal menù

--- app/src/main/java/com/example/andreea/eat_and_meet/CreateEvent.java
+++ app/src/main/java/com/example/andreea/eat_and_meet/CreateEvent.java
@@ -1,5 +1,6 @@
 package com.example.andreea.eat_and_meet;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
@@ -10,10 +11,12 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.TimePicker;
-
+import android.widget.DatePicker;
 import java.util.Calendar;
 
 public class CreateEvent extends AppCompatActivity implements View.OnClickListener, TimePickerDialog.OnTimeSetListener {
+
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class CreateEvent extends AppCompatActivity implements View.OnClickListen
         prosegui.setOnClickListener(this);
 
         findViewById(R.id.timeTextView).setOnClickListener(this);
+        findViewById(R.id.dateTextView).setOnClickListener(this);
     }
 
     public void onRadioButtonClicked(View view) {
@@ -50,10 +54,34 @@ public class CreateEvent extends AppCompatActivity implements View.OnClickListen
             TimePickerDialog dialog = new TimePickerDialog(this, this, hour, minute, true);
             dialog.show();
         }
-    }
 
+        if (view.getId() == R.id.dateTextView) {
+            //Mi prendo l'ora attuale
+            final Calendar c = Calendar.getInstance();
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            int month = c.get(Calendar.MONTH);
+            int year = c.get(Calendar.YEAR);
+
+            //Creo una dialog per prendere l'ora impostandogli l'ora attuale e iscrivendo
+            //la main activity all'evento di pick dell'ora
+            datePickerDialog = new DatePickerDialog(CreateEvent.this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+                            TextView date = (TextView) findViewById(R.id.dateTextView);
+                            // set day of month , month and year value in the edit text
+                            date.setText(dayOfMonth + "/"
+                                    + (monthOfYear + 1) + "/" + year);
+
+                        }
+                    }, year, month, day);
+            datePickerDialog.show();
+        }
+    }
     @Override
-    public void onTimeSet(TimePicker timePicker, int hour, int minutes) {
+    public void onTimeSet(TimePicker timepicker, int hour, int minutes) {
         //setto il testo della textview con l'ora e minuti presi dalla dialog
         TextView textView = (TextView) findViewById(R.id.timeTextView);
         textView.setText(String.format("%d:%d", hour, minutes));
