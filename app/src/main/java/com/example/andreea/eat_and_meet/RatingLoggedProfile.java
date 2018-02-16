a65d99da53189569660967ee26cd99de4178b3dd
andreturtoiu <andreturtoiu@hotmail.it>
2018-02-16 09:41:55 +0100

    Modifiche Rating

--- app/src/main/java/com/example/andreea/eat_and_meet/RatingLoggedProfile.java
+++ app/src/main/java/com/example/andreea/eat_and_meet/RatingLoggedProfile.java
@@ -10,25 +10,31 @@ import android.widget.RatingBar;
 
 public class RatingLoggedProfile{
     private String emailFrom; //chi ha scritto la recensione
+    private String emailTo; //Sul profilo di chi
     private String aboutYou;
     int numStars;
 
 
     public RatingLoggedProfile(){
         this.setEmailFrom("");
+        this.setEmailTo("");
         this.setAboutYou("");
         this.setNumStars(0);
 
     }
 
-    public  RatingLoggedProfile(String emailFrom,String aboutYou, int numStars){
+    public  RatingLoggedProfile(String emailFrom,String emailTo,String aboutYou, int numStars){
         setEmailFrom(emailFrom);
+        setEmailTo(emailTo);
         setAboutYou(aboutYou);
         setNumStars(numStars);
     }
 
     public void setEmailFrom(String emailFrom){this.emailFrom = emailFrom;}
     public String getEmailFrom(){return this.emailFrom;}
+
+    public void setEmailTo(String emailTo){this.emailTo = emailTo;}
+    public String getEmailTo(){return this.emailTo;}
 
     public int getNumStars(){return this.numStars;}
     public void setNumStars(int numStars) {
